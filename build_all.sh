761795ff6c3c5094938ee7ed71e22e47e82ba7ba
Mingjing Tong <mingjing.tong@noaa.gov>
2024-09-18 14:42:24 -0400

    Changes made to be able to compile on Gaea

--- build_all.sh
+++ build_all.sh
@@ -8,49 +8,66 @@
 
 set -eux
 
+# Get the root of the cloned ufs-utils directory
+if [[ $(uname -s) == Darwin ]]; then
+  readonly DIR_ROOT=$(cd "$(dirname "$(greadlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+else
+  readonly DIR_ROOT=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+fi
+
+source "${DIR_ROOT}/sorc/machine-setup.sh"
+
+# User Options
 target=${target:-"NULL"}
 compiler=${compiler:-"intel"}
-export MOD_PATH
 
 if [[ "$target" == "linux.*" || "$target" == "macosx.*" ]]; then
- unset -f module
- set +x
- source ./modulefiles/build.$target > /dev/null
- set -x
+  unset -f module
+  set +x
+  source "${DIR_ROOT}/modulefiles/build.${target}" > /dev/null
+  set -x
 else
- set +x
- source ./sorc/machine-setup.sh
- module use ./modulefiles
- module load build.$target.$compiler > /dev/null
- module list
- set -x
+  set +x
+  module use "${DIR_ROOT}/modulefiles"
+  module load "build.$target.$compiler" > /dev/null
+  module list
+  set -x
 fi
 
 # Ensure the submodules have been initialized.
 
-if [[ ! -d ./ccpp-physics/physics ]]; then
+if [[ ! -d "${DIR_ROOT}/ccpp-physics/physics" ]]; then
+  cd "${DIR_ROOT}"
   git submodule init
   git submodule update
 fi
 
+# Collect BUILD Options
+CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-Release}"
+
+# Install options; destination for built executables, libraries, CMake Package config
+CMAKE_FLAGS+=" -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX:-${DIR_ROOT}} -DCMAKE_INSTALL_BINDIR=${INSTALL_BINDIR:-exec}"
+
+# Testing options
 # The unit test data download is part of the build system. Not all machines can
 # access the EMC ftp site, so turn off the build (-DBUILD_TESTING=OFF) of the units tests accordingly.
 # Those with access to the EMC ftp site are: Orion and Hera.
-
-CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=../ -DCMAKE_INSTALL_BINDIR=exec -DBUILD_TESTING=OFF"
+CMAKE_FLAGS+=" -DBUILD_TESTING=${BUILD_TESTING:-OFF}"
 
 # Allow users of this script to provide CMake options e.g. -DGFS=ON|OFF to build GFS specific utilities only
 CMAKE_OPTS=${CMAKE_OPTS:-"-DGFS=ON"}
 
-rm -fr ./build
-mkdir ./build && cd ./build
+# Re-use or create a new BUILD_DIR (Default: create new BUILD_DIR)
+BUILD_DIR=${BUILD_DIR:-"${DIR_ROOT}/build"}
+[[ ${BUILD_CLEAN:-"YES"} =~ [yYtT] ]] && rm -rf "$BUILD_DIR"
+mkdir -p "${BUILD_DIR}" && cd "${BUILD_DIR}"
 
-cmake .. ${CMAKE_FLAGS} ${CMAKE_OPTS}
+cmake ${CMAKE_FLAGS} ${CMAKE_OPTS} "${DIR_ROOT}"
 
-make -j ${BUILD_JOBS:-8} VERBOSE=${BUILD_VERBOSE:-}
+make -j "${BUILD_JOBS:-8}" VERBOSE="${BUILD_VERBOSE:-}"
 make install
 
 #ctest
 #ctest -I 4,5
 
-exit
+exit 0
